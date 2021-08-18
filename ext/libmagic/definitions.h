c133aefb209cb4d7a6eef39baa6bb0729597cdb1
Sourav Goswami <souravgoswami@protonmail.com>
2021-08-19 04:18:42 +0530

    Remove unused macro

--- ext/libmagic/definitions.h
+++ ext/libmagic/definitions.h
@@ -2,7 +2,3 @@
 	magic_t *cookie ; \
 	TypedData_Get_Struct(self, magic_t, &fileType, cookie) ; \
 	if(!*cookie) rb_raise(rb_eFileClosedError, "Magic cookie already closed") ;
-
-#define RB_UNWRAP_OPEN(cookie) \
-	magic_t *cookie ; \
-	TypedData_Get_Struct(self, magic_t, &fileType, cookie) ;
