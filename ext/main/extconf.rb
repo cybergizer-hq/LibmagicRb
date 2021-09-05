98d3dde641979166e65ea23da2757d937778ff84
Sourav Goswami <souravgoswami@protonmail.com>
2021-09-05 16:46:19 +0530

    Rename libmagic to main for rake compiler

--- ext/main/extconf.rb
+++ ext/main/extconf.rb
@@ -6,4 +6,4 @@ $CFLAGS << ' -Os'
 abort "\e[1;31m*** Can't find magic.h ***\e[0m" unless have_header('magic.h')
 abort "\e[1;31m*** Can't find magic_open() in magic.h ***\e[0m" unless have_library('magic', 'magic_open')
 
-create_makefile 'libmagic_rb/main'
+create_makefile 'libmagic_rb/libmagic_rb'
