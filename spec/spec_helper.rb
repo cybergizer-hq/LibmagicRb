74204aa37f34cc3c258d5d917dbcdc19d86e2648
Sourav Goswami <souravgoswami@protonmail.com>
2021-09-05 17:29:41 +0530

    Use local libmagic_rb, installation unneeded for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
-require 'libmagic_rb'
+require './lib/libmagic_rb'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+	# Enable flags like --only-failures and --next-failure
+	config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
+	# Disable RSpec exposing methods globally on `Module` and `main`
+  	config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+	config.expect_with :rspec do |c|
+		c.syntax = :expect
+	end
 end
